5d96983e2cfcd90c69702a6b5aa5461ef8b36b37
Shadow Cat <shadowcatzero@protonmail.com>
2023-02-11 23:42:53 -0500

    no idea

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -48,6 +48,10 @@ local config = function()
         on_attach = on_attach,
         capabilities = capabilities,
     }
+    require'lspconfig'.kotlin_language_server.setup {
+        on_attach = on_attach,
+        capabilities = capabilities
+    }
 end
 
 return function(use)
