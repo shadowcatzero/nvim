b979d133209836335b774f57b5a8a217485ba332
Shadow Cat <shadowcatzero@protonmail.com>
2023-02-16 01:39:44 -0500

    added which-key

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -18,4 +18,16 @@ return {
         end,
         dependencies = 'nvim-lua/plenary.nvim'
     },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            })
+        end,
+    },
 }
