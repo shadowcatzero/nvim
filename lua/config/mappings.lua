e45eb4ba902ba440af95589ce133020b2b21e3ef
Shadow Cat <shadowcatzero@protonmail.com>
2023-06-03 23:48:06 -0400

    fix "ascension" (it's not really that cool)

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -72,6 +72,6 @@ lo.map("<C-"..l..">", "<C-w>l")
 lo.cmd("<C-,>", "res -1")
 lo.cmd("<C-.>", "res +1")
 
--- ascension
-m.map("x","<leader>p", "\"_dp")
+-- ascension (in <leader>p <leader>p, paste without replacing clipboard)
+m.map("x","<leader>p", "\"_dhp")
 
